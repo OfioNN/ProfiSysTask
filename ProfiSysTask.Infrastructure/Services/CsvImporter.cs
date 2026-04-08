1b5b6d1d7b832db77568a3fc544f71e2a560888c
Dawid Szymik <dawid_szymik@wp.pl>
2026-04-08 18:45:33 +0200

    feat: implement CsvImporter using CsvHelper

---
+++ ProfiSysTask.Infrastructure/Services/CsvImporter.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using ProfiSysTask.Core.Interfaces;
+using ProfiSysTask.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ProfiSysTask.Infrastructure.Services {
+    public class CsvImporter : ICsvImporter {
+        public IEnumerable<Document> Import(string documentsFilePath, string itemsFilePath) {
+
+            var csvConfig = new CsvConfiguration(CultureInfo.GetCultureInfo("pl-PL")) {
+                HasHeaderRecord = true,
+                Delimiter = ","
+            };
+
+            using var docReader = new StreamReader(documentsFilePath);
+            using var docCsv = new CsvReader(docReader, csvConfig);
+            var documents = docCsv.GetRecords<Document>().ToList();
+
+            using var itemReader = new StreamReader(itemsFilePath);
+            using var itemCsv = new CsvReader(itemReader, csvConfig);
+            var items = itemCsv.GetRecords<DocumentItem>().ToList();
+
+            foreach (var document in documents) {
+                document.Items = items.Where(i => i.DocumentId == document.Id).ToList();
+            }
+
+            return documents;
+        }
+    }
+}
